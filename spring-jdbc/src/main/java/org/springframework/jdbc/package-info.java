45645d43eab1e6080ae8b590ff76e87ca445f149
gitqiyun <1114260199@qq.com>
2019-06-26 23:30:08 +0800

    第一次提交

---
+++ spring-jdbc/src/main/java/org/springframework/jdbc/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * jdbc的封装，简化操作，类似于mybatis
+ * spring jdbc是一层很浅的封装，速度比mybatis快一点，但是封装过于简单也不一定就好，
+ * 大部分时候还是用mybatis，综合性能好点。
+ */
+@NonNullApi
+@NonNullFields
+package org.springframework.jdbc;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
