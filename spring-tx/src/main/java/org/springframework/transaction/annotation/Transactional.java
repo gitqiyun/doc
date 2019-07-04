b2a7f125e8cedb13b5ec875a09ba9fa1ce89457d
gitqiyun <1114260199@qq.com>
2019-07-04 17:39:33 +0800

    添加Transaction注解成员变量注释

--- spring-tx/src/main/java/org/springframework/transaction/annotation/Transactional.java
+++ spring-tx/src/main/java/org/springframework/transaction/annotation/Transactional.java
@@ -51,10 +51,10 @@ import org.springframework.transaction.TransactionDefinition;
  * @author Colin Sampaleanu
  * @author Juergen Hoeller
  * @author Sam Brannen
- * @since 1.2
  * @see org.springframework.transaction.interceptor.TransactionAttribute
  * @see org.springframework.transaction.interceptor.DefaultTransactionAttribute
  * @see org.springframework.transaction.interceptor.RuleBasedTransactionAttribute
+ * @since 1.2
  */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
@@ -64,6 +64,7 @@ public @interface Transactional {
 
 	/**
 	 * Alias for {@link #transactionManager}.
+	 *
 	 * @see #transactionManager
 	 */
 	@AliasFor("transactionManager")
@@ -75,115 +76,63 @@ public @interface Transactional {
 	 * matching the qualifier value (or the bean name) of a specific
 	 * {@link org.springframework.transaction.PlatformTransactionManager}
 	 * bean definition.
-	 * @since 4.2
+	 *
 	 * @see #value
+	 * @since 4.2
 	 */
 	@AliasFor("value")
 	String transactionManager() default "";
 
 	/**
-	 * The transaction propagation type.
-	 * <p>Defaults to {@link Propagation#REQUIRED}.
-	 * @see org.springframework.transaction.interceptor.TransactionAttribute#getPropagationBehavior()
+	 * 事务传播类型.
 	 */
 	Propagation propagation() default Propagation.REQUIRED;
 
 	/**
-	 * The transaction isolation level.
-	 * <p>Defaults to {@link Isolation#DEFAULT}.
-	 * <p>Exclusively designed for use with {@link Propagation#REQUIRED} or
-	 * {@link Propagation#REQUIRES_NEW} since it only applies to newly started
-	 * transactions. Consider switching the "validateExistingTransactions" flag to
-	 * "true" on your transaction manager if you'd like isolation level declarations
-	 * to get rejected when participating in an existing transaction with a different
-	 * isolation level.
-	 * @see org.springframework.transaction.interceptor.TransactionAttribute#getIsolationLevel()
-	 * @see org.springframework.transaction.support.AbstractPlatformTransactionManager#setValidateExistingTransaction
+	 * 事务隔离级别.
+	 * <p>默认为{@link Isolation＃DEFAULT}。
+	 * <p>专为{@link Propagation＃REQUIRED}或使用而设计
+	 * {@link Propagation #REQUIRES_NEW}，因为它仅适用于新启动的
+	 * 交易。考虑将“validateExistingTransactions”标志切换为
+	 * 如果您想要隔离级别声明，请在事务管理器上使用“true”
+	 * 在参与与其他交易的现有交易时被拒绝
+	 * 隔离级别。
 	 */
 	Isolation isolation() default Isolation.DEFAULT;
 
 	/**
-	 * The timeout for this transaction (in seconds).
-	 * <p>Defaults to the default timeout of the underlying transaction system.
-	 * <p>Exclusively designed for use with {@link Propagation#REQUIRED} or
-	 * {@link Propagation#REQUIRES_NEW} since it only applies to newly started
-	 * transactions.
-	 * @see org.springframework.transaction.interceptor.TransactionAttribute#getTimeout()
+	 * 此事务的超时（以秒为单位）。
+	 * <p>默认为基础交易系统的默认超时。
+	 * <p>专为{@link Propagation＃REQUIRED}或使用而设计
+	 * {@link Propagation #REQUIRES_NEW}，因为它仅适用于新启动的
+	 * 交易。
 	 */
 	int timeout() default TransactionDefinition.TIMEOUT_DEFAULT;
 
 	/**
-	 * A boolean flag that can be set to {@code true} if the transaction is
-	 * effectively read-only, allowing for corresponding optimizations at runtime.
-	 * <p>Defaults to {@code false}.
-	 * <p>This just serves as a hint for the actual transaction subsystem;
-	 * it will <i>not necessarily</i> cause failure of write access attempts.
-	 * A transaction manager which cannot interpret the read-only hint will
-	 * <i>not</i> throw an exception when asked for a read-only transaction
-	 * but rather silently ignore the hint.
-	 * @see org.springframework.transaction.interceptor.TransactionAttribute#isReadOnly()
-	 * @see org.springframework.transaction.support.TransactionSynchronizationManager#isCurrentTransactionReadOnly()
+	 * 读写或只读事务
 	 */
 	boolean readOnly() default false;
 
 	/**
-	 * Defines zero (0) or more exception {@link Class classes}, which must be
-	 * subclasses of {@link Throwable}, indicating which exception types must cause
-	 * a transaction rollback.
-	 * <p>By default, a transaction will be rolling back on {@link RuntimeException}
-	 * and {@link Error} but not on checked exceptions (business exceptions). See
-	 * {@link org.springframework.transaction.interceptor.DefaultTransactionAttribute#rollbackOn(Throwable)}
-	 * for a detailed explanation.
-	 * <p>This is the preferred way to construct a rollback rule (in contrast to
-	 * {@link #rollbackForClassName}), matching the exception class and its subclasses.
-	 * <p>Similar to {@link org.springframework.transaction.interceptor.RollbackRuleAttribute#RollbackRuleAttribute(Class clazz)}.
-	 * @see #rollbackForClassName
-	 * @see org.springframework.transaction.interceptor.DefaultTransactionAttribute#rollbackOn(Throwable)
+	 * 定义零（0）或更多异常{@link Class classes}（Class对象数组），必须是
+	 * {@link Throwable}的子类，指示哪些异常类型必须导致事务回滚。
+	 * 默认情况下，事务将在{@link RuntimeException}上回滚
 	 */
 	Class<? extends Throwable>[] rollbackFor() default {};
 
 	/**
-	 * Defines zero (0) or more exception names (for exceptions which must be a
-	 * subclass of {@link Throwable}), indicating which exception types must cause
-	 * a transaction rollback.
-	 * <p>This can be a substring of a fully qualified class name, with no wildcard
-	 * support at present. For example, a value of {@code "ServletException"} would
-	 * match {@code javax.servlet.ServletException} and its subclasses.
-	 * <p><b>NB:</b> Consider carefully how specific the pattern is and whether
-	 * to include package information (which isn't mandatory). For example,
-	 * {@code "Exception"} will match nearly anything and will probably hide other
-	 * rules. {@code "java.lang.Exception"} would be correct if {@code "Exception"}
-	 * were meant to define a rule for all checked exceptions. With more unusual
-	 * {@link Exception} names such as {@code "BaseBusinessException"} there is no
-	 * need to use a FQN.
-	 * <p>Similar to {@link org.springframework.transaction.interceptor.RollbackRuleAttribute#RollbackRuleAttribute(String exceptionName)}.
-	 * @see #rollbackFor
-	 * @see org.springframework.transaction.interceptor.DefaultTransactionAttribute#rollbackOn(Throwable)
+	 * 类名数组，必须继承自Throwable
 	 */
 	String[] rollbackForClassName() default {};
 
 	/**
-	 * Defines zero (0) or more exception {@link Class Classes}, which must be
-	 * subclasses of {@link Throwable}, indicating which exception types must
-	 * <b>not</b> cause a transaction rollback.
-	 * <p>This is the preferred way to construct a rollback rule (in contrast
-	 * to {@link #noRollbackForClassName}), matching the exception class and
-	 * its subclasses.
-	 * <p>Similar to {@link org.springframework.transaction.interceptor.NoRollbackRuleAttribute#NoRollbackRuleAttribute(Class clazz)}.
-	 * @see #noRollbackForClassName
-	 * @see org.springframework.transaction.interceptor.DefaultTransactionAttribute#rollbackOn(Throwable)
+	 * 定义哪些异常class不回滚
 	 */
 	Class<? extends Throwable>[] noRollbackFor() default {};
 
 	/**
-	 * Defines zero (0) or more exception names (for exceptions which must be a
-	 * subclass of {@link Throwable}) indicating which exception types must <b>not</b>
-	 * cause a transaction rollback.
-	 * <p>See the description of {@link #rollbackForClassName} for further
-	 * information on how the specified names are treated.
-	 * <p>Similar to {@link org.springframework.transaction.interceptor.NoRollbackRuleAttribute#NoRollbackRuleAttribute(String exceptionName)}.
-	 * @see #noRollbackFor
-	 * @see org.springframework.transaction.interceptor.DefaultTransactionAttribute#rollbackOn(Throwable)
+	 * 定义哪些异常类名不回滚
 	 */
 	String[] noRollbackForClassName() default {};
 
